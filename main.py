d4e4e3067b5c7ec6e2f6be7df3a879e50a125b22
Laurence Fernandez <Laurencerfernandez@gmail.com>
2025-01-06 21:02:14 -0700

    Updated asteroids game to include player object

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
-
+from circleshape import *
+from player import *
 
 def main():
 	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	Screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+	fps = pygame.time.Clock()
+	dt = 0
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,9 +16,13 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		pygame.Surface.fill(screen, (0, 0, 0))
+		player.update(dt)
+		Screen.fill("black")
+		player.draw(Screen)
 		pygame.display.flip()
 
+		#fps limit to 60
+		dt = fps.tick(60) / 1000
 
 
 if __name__ == "__main__":
